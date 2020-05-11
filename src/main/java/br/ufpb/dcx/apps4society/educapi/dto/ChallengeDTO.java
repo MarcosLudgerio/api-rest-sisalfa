8868a5eedacdfacec9347799b5128a3f63fb843d
MarcosLudgerio <raimundo.marcos@dce.ufpb.br>
2020-05-11 08:18:11 -0300

    change get all challenges by context

---
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/dto/ChallengeDTO.java
@@ -0,0 +1,95 @@
+package br.ufpb.dcx.apps4society.educapi.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.validation.constraints.NotEmpty;
+
+import br.ufpb.dcx.apps4society.educapi.domain.Context;
+import br.ufpb.dcx.apps4society.educapi.domain.User;
+import org.hibernate.validator.constraints.Length;
+
+import br.ufpb.dcx.apps4society.educapi.domain.Challenge;
+
+public class ChallengeDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	@NotEmpty(message="Required")
+	@Length(min=5, max=50, message="The size must be between 5 and 50 characters")
+	private String word;
+	
+	private String soundUrl;
+	private String videoUrl;
+	private String imageUrl;
+
+	private User creator;
+
+	private List<Context> contexts;
+	
+	public ChallengeDTO() {}
+	
+	public ChallengeDTO(Challenge obj) {
+		this.id = obj.getId();
+		this.word = obj.getWord();
+		this.soundUrl = obj.getSoundUrl();
+		this.videoUrl = obj.getVideoUrl();
+		this.imageUrl = obj.getVideoUrl();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getSoundUrl() {
+		return soundUrl;
+	}
+
+	public void setSoundUrl(String soundUrl) {
+		this.soundUrl = soundUrl;
+	}
+
+	public String getVideoUrl() {
+		return videoUrl;
+	}
+
+	public void setVideoUrl(String videoUrl) {
+		this.videoUrl = videoUrl;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public User getCreator() {
+		return creator;
+	}
+
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+	public List<Context> getContexts() {
+		return contexts;
+	}
+
+	public void setContexts(List<Context> contexts) {
+		this.contexts = contexts;
+	}
+}
---
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,54 @@
+package br.ufpb.dcx.apps4society.educapi.resources.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.ufpb.dcx.apps4society.educapi.services.exceptions.DataIntegrityException;
+import br.ufpb.dcx.apps4society.educapi.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error", System.currentTimeMillis());
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<StandardError> constraintViolationException(ConstraintViolationException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage() + " " + e.getCause().getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(JpaObjectRetrievalFailureException.class)
+	public ResponseEntity<StandardError> entityNotFoundException(JpaObjectRetrievalFailureException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage() + " " + e.getCause().getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+}
